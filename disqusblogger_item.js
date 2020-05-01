2b0d533718afbe2df8201030d6410eac6b6579da
sikolektor <51529208+sikolektor@users.noreply.github.com>
2020-05-01 20:13:19 +0800

    Update disqusblogger_item.js

--- disqusblogger_item.js
+++ disqusblogger_item.js
@@ -8,4 +8,13 @@
             dsq.src = '//' + disqus_shortname + '.disqus.com/blogger_item.js';
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
         })();
+            disqus_loaded = true;
+    };
+    window.onscroll = function(e) {
+        if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 800)) {
+            //hit bottom of page
+            if (disqus_loaded==false)
+                load_disqus()
+        }
+    };
 //]]>
