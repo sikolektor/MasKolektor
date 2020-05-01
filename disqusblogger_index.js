443718f8665f6085ba951164879818e79295b30f
sikolektor <51529208+sikolektor@users.noreply.github.com>
2020-05-01 20:08:14 +0800

    Update disqusblogger_index.js

--- disqusblogger_index.js
+++ disqusblogger_index.js
@@ -2,8 +2,11 @@
         /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
         var disqus_shortname = 'kolektorw3'; // required: replace username with your forum shortname
 
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
+    var disqus_loaded = false;
+    function load_disqus() {
+
+        // SIMPAN KODE JAVASCRIPT DISQUS DI BAWAH INI
+(function() {
             var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
             dsq.src = '//' + disqus_shortname + '.disqus.com/blogger_index.js';
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
