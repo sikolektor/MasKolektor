9b76edc16007169a8780792fac35b775a85a8a8c
sikolektor <51529208+sikolektor@users.noreply.github.com>
2020-05-01 20:10:02 +0800

    Update disqusblogger_item.js

--- disqusblogger_item.js
+++ disqusblogger_item.js
@@ -1,9 +1,9 @@
 //<![CDATA[
-        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = 'kolektorw3'; // required: replace username with your forum shortname
+    var disqus_loaded = false;
+    function load_disqus() {
 
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
+        // SIMPAN KODE JAVASCRIPT DISQUS DI BAWAH INI
+(function() {
             var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
             dsq.src = '//' + disqus_shortname + '.disqus.com/blogger_item.js';
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
