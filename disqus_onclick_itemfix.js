16f2a125b02b8f418b99f9657c86b94d73d55336
sikolektor <51529208+sikolektor@users.noreply.github.com>
2020-05-03 19:54:28 +0800

    Create disqus_onclick_itemfix.js

---
+++ disqus_onclick_itemfix.js
@@ -0,0 +1,8 @@
+(function() {
+  var bloggerjs = document.createElement('script');
+  bloggerjs.type = 'text/javascript'
+  bloggerjs.async = true;
+  bloggerjs.src = '//'+disqus_shortname+'.disqus.com/blogger_item.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bloggerjs);
+})();
+function load_Comments(){var e=document.getElementById("show-disqus");e.style.display="none";var t="kolektorw3";!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://"+t+".disqus.com/embed.js",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)}()}
