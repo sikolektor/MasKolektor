99c327e7f3e67fa1910cfc9973deb440e4f712a0
sikolektor <51529208+sikolektor@users.noreply.github.com>
2020-05-05 19:35:48 +0800

    Update and rename disqus_index.js to disqus_index.min.js

--- disqus_index.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var bloggerjs = document.createElement('script');
-  bloggerjs.type = 'text/javascript';
-  bloggerjs.async = true;
-  bloggerjs.src = '//'+disqus_shortname+'.disqus.com/blogger_index.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bloggerjs);
-})();
