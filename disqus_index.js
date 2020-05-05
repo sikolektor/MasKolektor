524b475186d2fd766c323b9c7e9de452686647cd
sikolektor <51529208+sikolektor@users.noreply.github.com>
2020-05-05 08:35:03 +0800

    Update disqus_index.js

--- disqus_index.js
+++ disqus_index.js
@@ -5,10 +5,3 @@
   bloggerjs.src = '//'+disqus_shortname+'.disqus.com/blogger_index.js';
   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bloggerjs);
 })();
-(function() {
-  var bloggerjs = document.createElement('script');
-  bloggerjs.type = 'text/javascript';
-  bloggerjs.async = true;
-  bloggerjs.src = '//'+disqus_shortname+'.disqus.com/count.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bloggerjs);
-})();
