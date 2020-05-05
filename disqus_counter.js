719cba52dac827a026f303cbfab3485927ae3ea4
sikolektor <51529208+sikolektor@users.noreply.github.com>
2020-05-05 08:35:54 +0800

    Create disqus_counter.js

---
+++ disqus_counter.js
@@ -0,0 +1,11 @@
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'kolektorw3';
+ 
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+  var bloggerjs = document.createElement('script');
+  bloggerjs.type = 'text/javascript';
+  bloggerjs.async = true;
+  bloggerjs.src = '//'+disqus_shortname+'.disqus.com/count.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(bloggerjs);
+})();
